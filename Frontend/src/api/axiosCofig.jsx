f6cf15877c68dbd4aeefb574c58a5825c872a86a
Farhan Sadik <farhansadik0760@gmail.com>
2025-06-26 22:13:18 +0600

    deploy

--- Frontend/src/api/axiosCofig.jsx
+++ Frontend/src/api/axiosCofig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  baseURL: "https://my-gadget-backend-3udq.onrender.com/",
 });
 
 export default instance;
